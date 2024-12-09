b887532f21580472645259149ce8fdf64923703a
100CallsToEurop <piaterik1231232mail.ru>
2024-12-09 23:34:03 +0300

    fix order

--- apps/billing/src/features/user/application/events/send-notification/letter-of-grief.ts
+++ apps/billing/src/features/user/application/events/send-notification/letter-of-grief.ts
@@ -1,2 +1,2 @@
 export const letterOfGrief = (orderId: number, fullName: string): string =>
-  `Hi ${fullName}, your order ${orderId} has been successfully placed! Thank you.`;
+  `Hi ${fullName}, your order ${orderId} has been cancelled due to a failed payment. Please check your payment information and try placing your order again.`;
