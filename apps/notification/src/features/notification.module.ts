18a3ace25b392f72b1e155f8fc336342591b0376
100CallsToEurop <piaterik1231232mail.ru>
2024-12-09 21:38:37 +0300

    fix data-source

--- apps/notification/src/features/notification.module.ts
+++ apps/notification/src/features/notification.module.ts
@@ -1,13 +1,13 @@
 import { AllExceptionsFilter } from '@app/common';
-import { JwtAuthGuard } from '@app/common/guards';
-import { JwtStrategy } from '@app/common/strategies';
 import { KafkaConfigService } from '@app/providers/kafka/config';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { APP_GUARD, APP_FILTER } from '@nestjs/core';
+import { APP_FILTER, APP_GUARD } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TypeOrmConfigService } from '../db/config';
 import { MessageModule } from './message/message.module';
+import { JwtAuthGuard } from '@app/common/guards';
+import { JwtStrategy } from '@app/common/strategies';
 
 @Module({
   imports: [
@@ -21,8 +21,8 @@ import { MessageModule } from './message/message.module';
   controllers: [],
   providers: [
     KafkaConfigService,
-    // JwtStrategy,
-    // { provide: APP_GUARD, useClass: JwtAuthGuard },
+    JwtStrategy,
+    { provide: APP_GUARD, useClass: JwtAuthGuard },
     {
       provide: APP_FILTER,
       useClass: AllExceptionsFilter,
